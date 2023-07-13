b0b9d6153ac8916ddc60cbeba2f9153298ea6500
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-07-13 20:04:03 +1000

    some code added

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
+let password = document.querySelector("#passwordText")
 
 function generateString() {
     for (let i = 0; i < characters.length; i++) {
-     
+
+        password.textContent += characters[i]
+
     }
 }_ 
 
+function randomString() {
+    let randomString = Math.floor (Math.random() *10)
+}
+
+
+// Math.floor (Math.random() *3)
